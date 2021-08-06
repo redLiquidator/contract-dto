3256832efd1684693bcc377558876832a24d8f4e
unknown <20210112@I-BCCARD.BIZ>
2021-08-06 17:56:11 +0900

    first

---
+++ src/main/java/com/contractdto/Contract.java
@@ -0,0 +1,19 @@
+package com.contractdto;
+public class Contract {
+	private String name;
+	private int contractNumber;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getContractNumber() {
+		return contractNumber;
+	}
+	public void setContractNumber(int contractNumber) {
+		this.contractNumber = contractNumber;
+	}
+	
+	
+}
---
+++ src/test/java/com/contractdto/contractdto/ContractDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contractdto.contractdto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
